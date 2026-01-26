5b01bd0df28ac3c85f7635c5c13175a7dc098ac2
ReechardDev <inemesitdavid90@gmail.com>
2026-01-26 03:13:50 -0700

    Fix complete favorites & recommendations

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,38 +1,15 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import SearchBar from './components/SearchBar';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
-import RecipeDetails from './components/RecipeDetails';
-import SearchBar from './components/SearchBar';
-import FavoritesList from './components/FavoritesList';
-import RecommendationsList from './components/RecommendationsList';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <nav>
-          <Link to="/">Home</Link>
-        </nav>
-
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <h1>Recipe Sharing App</h1>
-
-                <SearchBar />
-                <AddRecipeForm />
-                <RecipeList />
-
-                <FavoritesList />
-                <RecommendationsList />
-              </div>
-            }
-          />
-          <Route path="/recipes/:id" element={<RecipeDetails />} />
-        </Routes>
-      </div>
+      <nav><Link to="/">Home</Link></nav>
+      <Routes>
+        <Route path="/" element={<div><h1>HOME</h1><SearchBar /><AddRecipeForm /><RecipeList /></div>} />
+      </Routes>
     </BrowserRouter>
   );
 }
